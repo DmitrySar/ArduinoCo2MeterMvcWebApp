9859c93e90e5ceee8fabdb1e69ccc32864a3b463
DmitrySar <youshin.d@gmail.com>
2021-02-04 10:49:26 +0300

    bugfix, add last day for default value

--- src/main/java/graphics/graph/controller/DataController.java
+++ src/main/java/graphics/graph/controller/DataController.java
@@ -6,6 +6,7 @@ import graphics.graph.repository.TemperatureRepository;
 import graphics.graph.repository.TvocRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class DataController {
+
     @Autowired
     private CO2Repository co2Repository;
     @Autowired
@@ -31,10 +34,10 @@ public class DataController {
     }
 
     @GetMapping("/history")
-    public String getHistory(@RequestParam(defaultValue = "2021-02-03T00:00:01")
+    public String getHistory(@RequestParam(defaultValue = "#{T(java.time.LocalDateTime).now().minusDays(1)}")
                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                                          LocalDateTime start,
-                             @RequestParam(defaultValue = "2021-02-03T23:00:01")
+                             @RequestParam(defaultValue = "#{T(java.time.LocalDateTime).now()}")
                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                                      LocalDateTime stop,
                              Model model) {
@@ -42,6 +45,8 @@ public class DataController {
         model.addAttribute("temperatureList", temperatureRepository.findByTimeBetween(start, stop));
         model.addAttribute("humidityList", humidityRepository.findByTimeBetween(start, stop));
         model.addAttribute("tvocList", tvocRepository.findByTimeBetween(start, stop));
+        model.addAttribute("start", DateTimeFormatter.ISO_DATE_TIME.format(start));
+        model.addAttribute("stop", DateTimeFormatter.ISO_DATE_TIME.format(stop));
 
         return "history";
     }
